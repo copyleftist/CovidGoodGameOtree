eb4cdeac0615fc7b453f4db240fa07f6df175c75
getzneet <basilegarcia@gmail.com>
2021-04-22 19:18:38 +0200

    update

--- step1/tests.py
+++ step1/tests.py
@@ -1,12 +1,18 @@
 from otree.api import Currency as c
 from . import pages
 from step2._builtin import Bot
+import time
+import numpy as np
 
 
 class PlayerBot(Bot):
     def play_round(self):
         if self.round_number == 1:
+            # time.sleep(120)
             yield pages.Instruction1
+        time.sleep(1+np.random.random())
         yield pages.Disclose, dict(disclose=True)
+        time.sleep(1+np.random.random())
         yield pages.Contribute, dict(contribution=c(5))
+        time.sleep(5)
         yield pages.Results
