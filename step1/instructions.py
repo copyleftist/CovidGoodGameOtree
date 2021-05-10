a122e1f91de5c16cae1e5962c36e2e4ab3c5c267
getzneet <basilegarcia@gmail.com>
2021-05-10 14:19:23 +0200

    update

--- step1/instructions.py
+++ step1/instructions.py
@@ -34,11 +34,11 @@ panels = {
        'Your type will <b>remain</b> the same all along the game.<br>'
        'Note that different types have different multipliers (the latter is displayed on the character tshirt).'
        '<br><br><div align="center"><img src="/static/img/2_players.png"><br><br></div>'
-       'In each round you will be randomly matched with another player. A round is divided into 3 steps.<br>'
-       '1) You will be asked to either display or hide your multiplier, so that the other player will know your type or not.<br>'
-       '2) You will be asked to contribute (a certain number of points) to a public pot.<br>'
+       'In each round you will be randomly matched with another player. A round is divided into 3 steps:<br><br>'
+       '1) You will be asked if you want to either display or hide your multiplier, so that the other player will know your type or not.<br><br>'
+       '2) You will be asked to contribute (a certain number of points) to a public pot.<br><br>'
        '3) Your contribution will be multiplied according to your type and the points put in the public pot'
-       'will be equally distributed among the players. <br>'
+       ' will be equally distributed among the players. <br><br>'
        'Thereafter, you will continue to the next round.'
        '</p>',
 
@@ -49,8 +49,8 @@ panels = {
        ' in the contribution step (he will see your character on his screen). '
        'Conversely, if you choose to hide it, the <b>other player</b> will see your character hiding his multiplier'
        ' by means of a sign in the <b>contribution step</b>.<br>'
-       '<br>Suppose you were attributed the <b style="color: #5893f6">blue</b> type and the other player the <b style="color: #d4c84d">yellow</b>,'
-       'then there are 4 possible "disclosure" situations:<br><br>'
+       '<br>Suppose you were attributed the <b style="color: #5893f6">blue</b> type and the other player the <b style="color: #d4c84d">yellow</b>'
+       '. There are 4 possible "disclosure" situations:<br><br>'
        '<b>1) You both disclose</b> <br><br>' + two_players(1.2, 0.8, 1.2, 0.8) +
        '<b>2) You both hide</b> <br><br>' + two_players(None, None, '...', '...') +
        '<b>3) You disclose and the other player hides</b> <br><br>' + two_players(1.2, None, 1.2, '...') +
@@ -58,9 +58,12 @@ panels = {
        '<p>',
 
     4: '<p>'
-       'In the second step of the round, you will see the outcome of the previous decision, that is one of the 4 disclosure situations.'
-       'Also, in each round, you will be endowed with 10 points, and asked to contribute to the public pot. You will be able to do so '
-       'using a slider, which value corresponds to the number of points you want to put in the public pot.'
+       'In the second step of the round, you will see the outcome of the previous decision, that is one of the <b>4 "disclosure" situations</b>.'
+       ' Also, in each round, you will be <b>endowed with 10 points</b>, and asked to contribute to the public pot. You will be able to do so '
+       'using a slider, which value corresponds to the number of points you want to put in the public pot. <br>'
+       'To select a value on the slider you can either select with your mouse cursor, eitheir using your left and right arrow keys on your keyboard.'
+       'The maximum contribution is 10, while the minimun is 0.<br>'
+       '<br><div align="center"><img src="/static/img/contribute.gif"><br></div>'
        '</p>'
 }
 
