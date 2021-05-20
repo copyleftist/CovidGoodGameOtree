f91db0b56ec383d0a55492744ab8db28feb56c38
getzneet <basilegarcia@gmail.com>
2021-05-20 15:23:07 +0200

    update

--- settings.py
+++ settings.py
@@ -46,6 +46,21 @@ ROOMS = [
         display_name='Prolific experiment',
         #participant_label_file='_rooms/econ101.txt',
     ),
+    dict(
+        name='prolific2',
+        display_name='Prolific experiment',
+        #participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(
+        name='prolific3',
+        display_name='Prolific experiment',
+        #participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(
+        name='prolific4',
+        display_name='Prolific experiment',
+        #participant_label_file='_rooms/econ101.txt',
+    ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
 
